589a0a3b5cfca17c89d28318ad9fe26c648c83f5
Hai Feng Kao <haifeng@cocoaspice.in>
2020-10-07 04:56:19 +0800

    update pods

---
+++ BowLite/Classes/BowLite/BowLite/Headers.swift
@@ -0,0 +1,30 @@
+@_exported import BowLiteCore
+@_exported import BowLiteEffects
+@_exported import BowLiteOptics
+
+precedencegroup KleisliCompositionPrecedence {
+    associativity: left
+}
+
+infix operator >=>: KleisliCompositionPrecedence
+
+precedencegroup CompositionPrecedence {
+    associativity: left
+}
+
+infix operator <<<: CompositionPrecedence
+infix operator >>>: CompositionPrecedence
+
+precedencegroup PartialApplicationPrecedence {
+    associativity: left
+    higherThan: AssignmentPrecedence
+}
+
+infix operator |> : PartialApplicationPrecedence
+
+infix operator <- : AssignmentPrecedence
+prefix operator |<-
+
+// Using operator / to obtain prisms, as seen on
+// Pointfree's Case Paths: https://github.com/pointfreeco/swift-case-paths
+prefix operator /
---
+++ Example/Pods/Target Support Files/BowLite/BowLite-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BowLite : NSObject
+@end
+@implementation PodsDummy_BowLite
+@end
